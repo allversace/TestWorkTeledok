9249fd28ef30f73834034b6e538a6a7d139e1eef
allversace <hohlovartur414@gmail.com>
2023-10-07 21:39:40 +0300

    ...

--- WebApplication2/DTO/ClientAPI.cs
+++ WebApplication2/DTO/ClientAPI.cs
@@ -2,5 +2,9 @@
 
 public class ClientAPI
 {
-    
+    public int INN_client { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+    public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
 }
